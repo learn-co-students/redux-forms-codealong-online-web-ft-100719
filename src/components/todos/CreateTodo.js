25762336ed8ad34f75a007f39c8ae95d71deb376
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-03-02 17:44:28 -0500

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class CreateTodo extends Component {
   render() {
-    return(
-      <div>
-        Create Todo Component
-      </div>
-    )
+    return <div>Create Todo Component</div>;
   }
 }
 
